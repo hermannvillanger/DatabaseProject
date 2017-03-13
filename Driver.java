ee44d4b3ffef71d0ee96911d470c180861b8a4f8
Magnleik <magnleik@stud.ntnu.no>
2017-03-13 11:51:51 +0100

    added delete methods and addExerciseToTemplate

--- Driver.java
+++ Driver.java
@@ -6,7 +6,24 @@ import java.util.Scanner;
 public class Driver {
 	static Scanner scanner;
 	static Connection myConn; 
-	
+	private String DeleteExercise(String exercise) throws SQLException{
+		return "delete * from exercise where Exercise_Name="+exercise;
+	}
+	private String DeleteWorkout(String workout_start) throws SQLException{
+		return "delete * from Workout where Workout_Start=" + workout_start;
+	}
+	private String DeleteTemplate(String template_id) throws SQLException{
+		return "delete * from Template where Template_Id=" + template_id;
+	}
+	private String DeleteNote(String note)throws SQLException{
+		return "delete * from Note where Note_Id="+note;
+	}
+	private String DeleteGoal(String goal) throws SQLException{
+		return "delete * from Goal where Goal_Id=" + goal;
+	}
+	private String DeleteGPS(String Gps){
+		return "delete * from GPS where GPS_Time="+Gps;
+	}
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
